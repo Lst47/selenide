120444e4c34174b6f60756e6b90211cc54b091f6
lst47 <linh20121@gmail.com>
2022-12-13 00:15:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/mwpvpka4qgp2niny?svg=true)](https://ci.appveyor.com/project/Lst47/selenium)
---
+++ src/test/java/FormTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class FormTest {
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
+    @Test
+    void positiveFormTest() {
+        String date = generateDate(4);
+        open("http://localhost:9999");
+        $("[data-test-id= city] input").setValue("Москва");
+        $("[data-test-id= date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= date] input").setValue(date);
+        $("[data-test-id= name] input").setValue("Андрей");
+        $("[data-test-id= phone] input").setValue("+79012345678");
+        $("[data-test-id= agreement] ").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(exist);
+    }
+}
