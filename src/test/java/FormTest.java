823075df8198899a38047b96363b7dff4935320e
lst47 <linh20121@gmail.com>
2023-01-11 18:06:20 +0300

    fix visible

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -7,8 +7,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -30,6 +29,22 @@ public class FormTest {
         $("[data-test-id= phone] input").setValue("+79012345678");
         $("[data-test-id= agreement] ").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(exist);
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+
+    @Test
+    void dropDownTest() {
+        String date = generateDate(4);
+        open("http://localhost:9999");
+        $("[data-test-id= city] input").setValue("Москва");
+        $("[data-test-id= date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= date] input").setValue(date);
+        $("[data-test-id= name] input").setValue("Андрей");
+        $("[data-test-id= phone] input").setValue("+79012345678");
+        $("[data-test-id= agreement] ").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldHave(exactText("Успешно! " + "Встреча успешно забронирована на " + date), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
+
